d1111d8b54c0918999836b2d651ab86680fa46d0
peter fotis <pfotis@gmail.com>
2021-08-12 20:47:13 +1000

    create style.css

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const path = require('path')
 const express = require('express')
 
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(path.join(publicDirectoryPath)))
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     res.send([{

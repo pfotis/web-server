df0ae9f764ad2b5c4e6546f760c55d46c8119007
peter fotis <pfotis@gmail.com>
2021-08-12 20:24:04 +1000

    create html files

--- src/app.js
+++ src/app.js
@@ -10,18 +10,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(path.join(publicDirectoryPath)))
 
-
-// app.get('/help', (req, res) => {
-//     res.send({
-//         name: 'Peter',
-//         age: 36
-//     })
-// })
-
-// app.get('/about', (req, res) => {
-//     res.send('<h1>About</h1>')
-// })
-
 app.get('/weather', (req, res) => {
     res.send([{
         city: 'Melbourne',

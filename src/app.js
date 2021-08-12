e7fbc2d747c8373c710ac60bd37f0512c36574d6
peter fotis <pfotis@gmail.com>
2021-08-12 21:23:12 +1000

    create about.hbs

--- src/app.js
+++ src/app.js
@@ -11,6 +11,20 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Peter Fotis'
+    })
+})
+
+app.get('/about', (req, res)=> {
+    res.render('about', {
+        title: 'About',
+        name: 'Peter Fotis'
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send([{
         city: 'Melbourne',

8f5942e03d2210bc7563372003e634852ebcd0b3
peter fotis <pfotis@gmail.com>
2021-08-17 23:36:41 +1000

    almost complete the weather app

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,13 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'From javascript'
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''    
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
